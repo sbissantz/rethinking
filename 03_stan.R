688ef8ed86c0c28305d9b397713ba060c56a9fd7
Steven Marcel Bißantz <steven.bissantz@gmail.com>
2021-12-06 19:43:28 +0100

    PPS

--- 03_stan.R
+++ 03_stan.R
@@ -17,7 +17,7 @@ N <- 1e3 ; steps <- 12
 pos <- replicate(N, prod(1 + runif(steps, 0, 0.1)))
 hist(pos)
 plot(density(pos))
-#
+
 N <- 1e3 ; steps <- 12
 # Normal
 small <- replicate(N, prod(1 + runif(steps, 0, 0.01)))
@@ -38,7 +38,81 @@ plot(density(log.big))
 #
 library(rethinking)
 data(Howell1) ; d <- Howell1
-str(d) ; precis(d)
+rethinking::precis(d)
+d2 <- d[d$age >18,]
+
+# Model
+# h ~ Normal(mu, sigma)
+# mu ~ Normal(178, 20) 
+# sigma ~ Uniform(0, 50)
+
+# Priors
+#
+# Prior for mu
+curve( dnorm(x, 178, 20), from=100, to=250 )
+# Prior for sigma
+curve( dunif(x, 0, 50), from=-10, to=60 )
+
+# Prior predictive simulation
+#
+N <- 1e3
+# Prior for mu
+sample_mu <- rnorm(N, 178, 20) 
+# Prior for sigma
+sample_sigma <- runif(N, 0, 50) 
+# Joint prior
+prior_h <- rnorm(N, sample_mu, sample_sigma)
+# Visualize the joint prior
+plot(density(prior_h))
+
+# Model
+#
+dat_ls <- list(
+                 N = length(d2$height),
+                 h = d2$height
+)
+
+model.stan <- "
+data{
+    int<lower=0> N;
+    real<lower=0> h[N];
+}
+parameters{
+   real mu; 
+   real sigma; 
+}
+model{
+    h ~ normal(mu, sigma);
+    mu ~ normal(170, 20);
+    sigma ~ uniform(0, 50);
+}
+"
+fit <- rstan::stan(model_code = model.stan, data = dat_ls)
+samples <- rstan::extract(fit)
+
+# Plot: Marginal posterior densities
+#
+plot( density(samples$mu) )
+mu <- mean(samples$mu)
+sigma <- sd(samples$mu)
+curve(dnorm(x, mu, sigma), add=TRUE, lty=2)
+
+# Plot: Marginal posterior densities
+#
+plot( density(samples$sigma) )
+mu <- mean(samples$sigma)
+sigma <- sd(samples$sigma)
+curve(dnorm(x, mu, sigma), add=TRUE, lty=2)
+
+# Plot: Joint posterior distribution
+#
+plot( samples$mu, samples$sigma, pch=20, cex=0.3)
+
+
+rethinking::dens
+
+
+
 
 
 
