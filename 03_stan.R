fbf2e9418d218abbbd2c21d41ce6e101be8a0c13
Steven Marcel Bißantz <41154100+sbissantz@users.noreply.github.com>
2021-12-06 18:46:15 +0100

    start chapter 4

--- 03_stan.R
+++ 03_stan.R
@@ -1,9 +1,10 @@
 options(mc.cores = parallel::detectCores())
 rstan::rstan_options(auto_write = TRUE)
 
-# Normal by addition
-# Fluctuationphenomenon
+# Fluctuation phenomenon --------------------------------------------------
 
+# Normal by addition
+#
 N <- 1e3 ; steps <- 16
 pos <- replicate(N, sum(runif(steps, -1, 1)))
 
@@ -16,13 +17,14 @@ N <- 1e3 ; steps <- 12
 pos <- replicate(N, prod(1 + runif(steps, 0, 0.1)))
 hist(pos)
 plot(density(pos))
-
+#
 N <- 1e3 ; steps <- 12
 # Normal
 small <- replicate(N, prod(1 + runif(steps, 0, 0.01)))
 # Not Normal
 big <- replicate(N, prod(1 + runif(steps, 0, 0.5)))
-op <- mfrow(c(1,1))
+# Visualize
+op <- par(mfrow=c(1,1))
 par(mfrow=c(2,1))
     plot(density(small)) ; plot(density(big))
 par(op)
@@ -32,10 +34,11 @@ par(op)
 log.big <- replicate(N, log(prod(1 + runif(steps, 0, 0.5))))
 plot(density(log.big))
 
-
-
-
-
+# Data
+#
+library(rethinking)
+data(Howell1) ; d <- Howell1
+str(d) ; precis(d)
 
 
 
