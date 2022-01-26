d07cb71cec015d281cf56c1f509278b0be092557
Steven Marcel Bißantz <steven.bissantz@gmail.com>
2022-01-26 18:26:34 +0100

    Estimated the posterior predictive contrast distribution

--- sim_interventions.R
+++ sim_interventions.R
@@ -34,9 +34,14 @@ plot(dag)
 library(rethinking)
 data(Howell1)
 d <- Howell1
-d$W <- d$weight
+d <- d[d$age>=18,]
+d$W <- scale(d$weight)
 d$S <- d$male + 1
-d$H <- d$height
+# d$H <- d$height
+
+plot(d$W, col=ifelse(d$S==1, "pink", "blue"), pch=20)
+abline(h=c(mean(d$W[d$S==1])))
+abline(h=c(mean(d$W[d$S==2])))
 
 # Q2: causal effect of S on W 
 # Functional relationship: W=f_W(S)
@@ -48,7 +53,8 @@ d$H <- d$height
 
 # Data list
 #
-dat_ls <- list(N=nrow(d), K=2, W=d$W, S=d$S)
+dat_ls <- list(N=nrow(d), K=2, W=as.numeric(d$W), S=d$S) 
+# dat_ls <- list(N=nrow(d), K=2, W=as.numeric(d$W, S=d$S, X=rep(0, nrow(d)))
 # Fitting
 #
 file <- file.path( getwd(), "stan", "mdl_1_sim_intervent.stan" )
@@ -65,13 +71,23 @@ fit$print()
 samples <- fit$draws(format="matrix")
 
 # Contrast distribution (mean weight)
-#
-mu_cont <- samples[,"alpha[1]"] - samples[,"alpha[2]"]
+# Note: + because alpha|1] > 0
+mu_cont <- samples[,"alpha_S[2]"] + samples[,"alpha_S[1]"]
+
 # Causal contrast in means (men, women)
 plot(density(mu_cont))
 
 # Contrast distribution (predicted weight) 
-# (Pointwise differences in PPDs)
+# (samplewise differences in PPDs)
+#
+N <- 1e4
+W_tilde_1 <- rnorm(N, samples[,"alpha_S[1]"], samples[,"sigma"])
+W_tilde_2 <- rnorm(N, samples[,"alpha_S[2]"], samples[,"sigma"])
+dens(W_tilde_1, col="red") ; dens(W_tilde_2, col="blue", add=TRUE)
+abline(v = colMeans(samples[,"alpha_S[1]"]), lty=2)
+abline(v = colMeans(samples[,"alpha_S[2]"]), lty=2)
+
+# Note: Try Posterior predictions with STAN
 #
 
 # Proportion above zero
