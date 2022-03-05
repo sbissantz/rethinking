6d2f21f1f611f2a36ab0fe793f47196d43b04348
Steven Marcel Bißantz <steven.bissantz@gmail.com>
2022-03-05 11:35:36 +0100

    Finished the case control bias simulation

--- sim_case_control_bias.R
+++ sim_case_control_bias.R
@@ -18,15 +18,21 @@
 # sigma ~ exponential(1)
 
 f <- function(N=100, b_XY=1, b_YZ=1) {
-  N <- 1e3
   X <- rnorm(N)
-  Y <- rnorm(b_XY*X)
-  Z <- rnorm(b_YZ*Y)
-  b_X <- lm((Y ~ X))['X']
-  b_XZ <- lm(Y ~ X + Z)['X']
+  Y <- rnorm(N, b_XY*X)
+  Z <- rnorm(N, b_YZ*Y)
+  b_X <- coef(lm(Y ~ X))['X']
+  b_XZ <- coef(lm(Y ~ X + Z))['X']
   c(b_X, b_XZ)
 }
 sim <- mcreplicate::mc_replicate(1e4, f(), mc.cores=4)
+
+plot(density(sim[1,]), ylim=c(0,5), xlim=c(0,1.5), lwd=3, 
+             xlab="Posterior mean")
+lines(density(sim[2,], lwd=3), lwd=3, col="red")
+text(x=0.5, 5, "wrong") ; text(x=1, 5, "correct")
+
+
 
 
 
