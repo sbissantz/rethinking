20da0f10feea06a1ebadd78c74c5f08041d6b92d
Steven Marcel Bißantz <steven.bissantz@gmail.com>
2022-04-08 19:53:38 +0200

    Sourced more steps out

--- wofls/prep.R
+++ wofls/prep.R
@@ -22,3 +22,37 @@ text(x=masskg, y=brainvolcc+50, sppnames)
 d$M <- with(d, (mass - mean(mass))/sd(mass))
 # Normalize
 d$B <- with(d, brain/max(brain))
+
+# Modifications 
+#
+# Standardize
+d$M <- with(d, (mass - mean(mass))/sd(mass))
+# Normalize
+d$B <- with(d, brain/max(brain))
+
+# Reduction
+#
+dat_ls <- list(N=nrow(d), B=d$B, M=d$M)
+
+# Model sketch
+#
+# B_i ~ Normal(mu_i, sigma)
+# mu_i = alpha + beta_M * M_i
+# alpha ~ normal(0,0.1)
+# beta_M ~ normal(0,5)
+# sigma ~ exponential(1)
+path <- "~/projects/stanmisc/wofls/"
+file <- file.path(path, "model.stan")
+
+# Fitting
+#
+# Compile the Stan program
+mdl <- cmdstanr::cmdstan_model(file, pedantic=TRUE)
+# Fit the model
+fit <- mdl$sample(dat=dat_ls)
+
+# Diagnostics
+#
+fit$cmdstan_diagnose()
+fit$print()
+
