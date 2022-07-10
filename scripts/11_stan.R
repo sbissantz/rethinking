e13f7659c74b591eb7a5c99d0d6453ce014b7d20
Steven Marcel Bißantz <steven.bissantz@gmail.com>
2022-07-10 11:26:15 +0200

    Suggest a PPS wofl
    Suggest a PPS wofl for a logistic regression.
    
    Todo: Check if it makes sense

--- scripts/11_stan.R
+++ scripts/11_stan.R
@@ -23,22 +23,23 @@ d$treatment <- 1 + d$prosoc_left + 2*d$condition
 # Prior implications (R)
 N <- 1e3
 
-alpha_lo <- rnorm(N, 0, 1.5)
-alpha_p <- plogis(alpha_lo) 
-plot(density(alpha_p), xlim=c(0,1))
-
-beta_lo <- rnorm(N, 0,0.5)
-beta_p <- plogis(beta_lo) 
-plot(density(beta_p), xlim=c(0,1))
-
-# Alternative
+# Helper function
+#
 sigmoid <- function(x) 1 / (1 + exp(-x))
+# Alpha prior
+alpha_lo <- rnorm(N, 0, 1.5)
 alpha_p <- sigmoid(alpha_lo)
+#alpha_p <- plogis(alpha_lo) 
+plot(density(alpha_p), xlim=c(0,1))
+# Beta prior
+beta_lo <- rnorm(N, 0,0.5)
 beta_p <- sigmoid(beta_lo) 
+#beta_p <- plogis(beta_lo) 
+plot(density(beta_p), xlim=c(0,1))
 
 # Visualize
 # logit(pi) = X_i*beta + alpha
-# pi = logit^-1(X_i*beta + alpha)
+# pi = logit^-1(X_i*beta + alpha) = sigmoid(X_i*beta + alpha)
 plot(c(-4,4), c(0,1), type="n", ylab="Pr(pull_left)",
      xlab="Predictor values", main="Prior predictive simulation")
 for(i in 1:50) {
@@ -47,14 +48,21 @@ for(i in 1:50) {
 
 
 
-library("bayesplot")
-library("rstanarm")
+# Stan Workflow
+#
+path <- "~/projects/stanmisc"
+file <- file.path(path, "stan", "11", "pps_1.stan") 
+mdl <- cmdstanr::cmdstan_model(file, pedantic=TRUE)
 
-##########################
+# 504 trials 
+dat_ls <- list(N=nrow(d), x=d$treatment)
+fit <- mdl$sample(data=dat_ls, fixed_param=TRUE)
 
+fit$cmdstan_diagnose()
+fit$summary()
 
-
-
+# Samples from the posterior 
+samples <- fit$draws(format="data.frame")
 
 # Prior implications
 #
@@ -63,39 +71,22 @@ library("rstanarm")
 alpha_lo <- samples$alpha
 beta_lo <- samples$beta
 
-# alpha (prob) - R version
-alpha_p <- plogis(alpha_lo)       # NEAT! 
-beta_p <- plogis(beta_lo)       # NEAT! 
+# Helper function 
+sigmoid <- function(x) 1 / (1 + exp(-x))
 
-# General version
-inv_logit <- function(x) exp(x) / (1 + exp(x))
-alpha_p <- inv_logit(alpha_lo)
+# alpha (prob)
+alpha_p <- sigmoid(alpha_lo) 
 plot(density(alpha_p))
-beta_p <- inv_logit(beta_lo)
+#alpha_p <- plogis(alpha_lo) 
+beta_p <- sigmoid(beta_lo)  
 plot(density(beta_p))
+#beta_p <- plogis(beta_lo) 
 
-# Samples
-#
+# Simulated data sets
+y_tilde <- fit$draws("y_tilde", format="matrix")
 
-
-
-#
-# Code a Stan version. Switching to Stan is necessary, because it allows to
-# constraint the p's more easily. E.g. a lower bound for the predictor.
-#
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# Prior preditive distribution
+plot(density(y_tilde[1,]), ylim=c(0,35), main="Prior predictive", xlab="y_tilde")
+for(i in 1:500) {
+  lines(density(y_tilde[i,]), col=col.alpha("black", 0.2))
+}
