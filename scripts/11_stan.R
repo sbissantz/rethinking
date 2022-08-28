a2dda2df4a0d4e567bc802ab7f9be07d4dee0e1e
Steven Marcel Bißantz <steven.bissantz@gmail.com>
2022-08-28 11:36:10 +0200

    Finish the chapter

--- scripts/11_stan.R
+++ scripts/11_stan.R
@@ -1373,6 +1373,9 @@ fit$summary()
 # Samples
 #
 (post <- fit$draws(format="matrix"))
+
+# Predictions
+#
 # S3 is the pivot
 s1 <- post[,"alpha[1]"]  +  post[,"beta"] * income[1]
 s2 <- post[,"alpha[2]"]  +  post[,"beta"] * income[2]
@@ -1383,4 +1386,85 @@ p <- sapply(seq(N_samples), function(i) softmax(c(s1[i], s2[i], 0)))
 p_double <- sapply(seq(N_samples), function(i) softmax(c(s1[i], s2_double[i], 0)))
 p_diff <- p_double[2,] - p[2,]
 mean(p_diff)
+
+# 
+# Predictor matched to observations
+#
+N <- 5e2
+# Simulate family income for each type of event
+family_income <- runif(N)
+# Assign a unique coefficient for each type of event
+b <- c(-2,0,2)
+career <- rep(NA, N) #empty vector of choices for each iindividual
+for (i in 1:N) {
+  score <- 0.5*(1:3) + b*family_income[i]
+  p <- softmax(c(score[1], score[2], score[3]))
+  career[i] <- sample( 1:3, size=1, prob=p )
+}
+
+# Data reduction
+#
+dat_ls <- list(N=N, K=3, career=career, family_income=family_income)
+
+# Fit the model
+#
+path <- "~/projects/stanmisc"
+file <- file.path(path, "stan", "11", "mdl_11.stan") 
+mdl <- cmdstanr::cmdstan_model(file, pedantic=TRUE)
+fit <- mdl$sample(dat_ls)
+
+# Diagnostics
+# Note: 1% divergent transitions
+fit$cmdstan_diagnose()
+fit$summary()
+
+# Posterior
+#
+post <- fit$draws(format="matrix")
+
+# S3 is the pivot
+N <- length(family_income)
+s1 <- sapply(seq(N), function(i) post[,"alpha[1]"]  +  
+             post[,"beta[1]"] * family_income[i])
+s2 <- sapply(seq(N), function(i) post[,"alpha[2]"]  +  
+             post[,"beta[2]"] * family_income[i])
+# Doubling the family outcome
+s1_new <- sapply(seq(N), function(i) post[,"alpha[1]"]  +  
+                 post[,"beta[1]"] * family_income[i] * 3)
+p <- sapply(seq(N_samples), function(i) softmax(c(s1[i], s2[i], 0)))
+# Scores with doubled family income
+p_new <- sapply(seq(N_samples), function(i) softmax(c(s1_new[i], s2[i], 0)))
+rowMeans(p_new) - rowMeans(p)
+
+#
+# Multinomial as Poisson
+#
+library(rethinking)
+data(UCBadmit)
+d <- UCBadmit
+
+# Reduction
+#
+dat_ls <- list( N=nrow(d), admit=d$admit, rejct = d$reject )
+
+# Fit the model
+#
+path <- "~/projects/stanmisc"
+file <- file.path(path, "stan", "11", "mdl_12.stan") 
+mdl <- cmdstanr::cmdstan_model(file, pedantic=TRUE)
+fit <- mdl$sample(dat_ls)
+
+# Diagnostics
+#
+fit$cmdstan_diagnose()
+fit$summary()
+
+# Posterior
+#
+post <- fit$draws(format="data.frame")
+calc_p <- function(x1, x2) {
+  exp(x1) / (exp(x1) + exp(x2))
+}
+(p_admit <- calc_p(post$alpha_1, post$alpha_2))
+mean(p_admit)
 
