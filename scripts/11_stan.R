bac7f7cfc1c0a138ea6d8a01b19676f7d0b0f27b
Steven Marcel Bißantz <steven.bissantz@gmail.com>
2022-09-21 10:13:25 +0200

    Update 11_stan.R

--- scripts/11_stan.R
+++ scripts/11_stan.R
@@ -1550,10 +1550,77 @@ fit$print()
 # If you should ever do a survival analysis, make sure, you understand the
 # deviations before making inferences from your models.
 
+#
+# Zero-inflated  Poisson 
+#
 
+# Generative simulation
+#
+# Bernoulli process (drink vs. work)
+prob_drink <- .2 #20% of the days monks drink, 80% the work
+rate_work <- 1  # Average of 1 manuscript per day
+# Sample 1 year of production
+N <- 365
+# Simulate days monk drinks
+set.seed(365)
+# Drink indicator (decision: drink 0/1)
+drink <- rbinom(N, 1, prob_drink) # Bernoulli
+# Simulate completed manuscripts
+y <- (1-drink)*rpois(N, rate_work)
+plot(table(y), xlab="manuscripts completed")
 
+# Model sketch
+#
+# y_i ~ ZIPoisson(p_i, lambda_i)
+# logit(p) = X_i beta_p + alpha_p
+# log(lambda) = X_i beta_l + alpha_l
+# a_p ~ normal(-1, 0.5)
+# a_l ~ normal(0, 0.5)
 
+# PPS
+#
+# inv_logit: logistic function
+alpha_p <- plogis(alpha)
+hist(alpha_p, main="Average probability of drinking")
+mtext(paste0("mean= ", round(mean(alpha_p), digits=2)))
 
+N <- 1e3
+lambda_log <- rnorm(N, 0, 0.5)
+# inv_log: exp function
+lambda <- exp(lambda_log)
+hist(lambda, main="Average n° manuscripts per day")
+mtext(paste0("mean= ", round(mean(lambda), digits=2)))
 
+# Data
+#
+dat_ls <- list("N"=length(y), "y"=as.integer(y))
+
+# Fit the model
+#
+# Ulam
+m12.4_ulam <- rethinking::ulam(
+                  alist(
+                        y|y>0 ~ custom(log1m(p) + poisson_lpmf(y|lambda)),
+                        y|y==0 ~ custom(log_mix(p, 0, poisson_lpmf(0|lambda))),
+                        logit(p) <- a_p,
+                        log(lambda) <- a_l,
+                        a_p ~ dnorm(-1.5,1),
+                        a_l ~ dnorm(1,0.5)
+                  ), data=list("y"=as.integer(y)), chains=4)
+rethinking::precis(m12.4_ulam)
+
+# Adapted Stan version
+path <- "~/projects/stanmisc"
+file <- file.path(path, "stan", "11", "mdl_14.stan") 
+mdl <- cmdstanr::cmdstan_model(file, pedantic=TRUE)
+fit <- mdl$sample(data=dat_ls)
+print(fit)
+
+# Stan (more readable)
+path <- "~/projects/stanmisc"
+file <- file.path(path, "stan", "11", "mdl_14b.stan") 
+mdl <- cmdstanr::cmdstan_model(file, pedantic=TRUE)
+fit <- mdl$sample(data=dat_ls)
+print(fit)
 
 
