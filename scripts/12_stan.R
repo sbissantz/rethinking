fb3eb0bd8d0fb41ab4418bc79d1f159cb0ff121e
Steven Marcel Bißantz <steven.bissantz@gmail.com>
2022-10-06 18:50:04 +0200

    Update 12_stan.R

--- scripts/12_stan.R
+++ scripts/12_stan.R
@@ -1,12 +1,20 @@
 #
 # Ch.12: Monsters and Mixtures
 #
-
 library(rethinking)
+#
+# Data
+#
 data(UCBadmit)
 d <- UCBadmit
+#
+# Data Wrangling
 d$gid <- ifelse(d$applicant.gender=="male",1L, 2L)
+#
+# Data list 
 dat <- list(A=d$admit, N=d$applications, gid=d$gid)
+#
+# Fit the model
 m12.1 <- ulam( 
               alist(
                     A ~ dbetabinom(N, pbar, theta),
@@ -15,10 +23,9 @@ m12.1 <- ulam(
                     transpars> theta <<- phi + 2.0,
                     phi ~ dexp(1)
               ), data=dat, chains=4)
-
+# 
+# Show stancode
 stancode(m12.1)
-# Phi-Kappa Reparameterized Beta-Binomial
-#
 # data{
 #     int N[12];
 #     int A[12];
@@ -29,45 +36,30 @@ stancode(m12.1)
 #     real<lower=0> phi;
 # }
 # transformed parameters{
+#     real theta;
+#     theta = phi + 2;
+# }
+# model{
 #     vector[12] pbar;
+#     phi ~ exponential( 1 );
+#     a ~ normal( 0 , 1.5 );
 #     for ( i in 1:12 ) {
 #         pbar[i] = a[gid[i]];
 #         pbar[i] = inv_logit(pbar[i]);
 #     }
-#     real theta = phi + 2;
-#     real alpha = pbar*theta;
-#     real beta = (1-pbar)*theta;
+#     A ~ beta_binomial( N , pbar*theta , (1-pbar)*theta );
 # }
-# model{
-#     phi ~ exponential( 1 );
-#     a ~ normal( 0 , 1.5 );
-#     A ~ beta_binomial( N , alpha, beta );
-# }
+# 
 
-# Phi-Kappa Reparameterized Beta-Binomial
-# ..-consistent with the distribution explorer website
+# Reduced data list
+dat_ls <- list("N"=nrow(d),"A"=d$admit, "N_bern"=d$applications, "gid"=d$gid,
+               "gno"=length(unique(dat$gid)))
+
 #
-# data{
-#     int N[12];
-#     int A[12];
-#     int gid[12];
-# }
-# parameters{
-#     vector[2] a;
-#     real<lower=0> phi;
-# }
-# transformed parameters{
-#     vector[12] phi;
-#     for ( i in 1:12 ) {
-#         phi[i] = a[gid[i]];
-#         phi[i] = inv_logit(phi[i]);
-#     }
-#     real kappa = phi + 2;
-#     real alpha = phi*kappa;
-#     real beta = (1-phi)*kappa;
-# }
-# model{
-#     phi ~ exponential( 1 );
-#     a ~ normal( 0 , 1.5 );
-#     A ~ beta_binomial( N , alpha, beta );
-# }
+# Fit the model
+path <- "~/projects/stanmisc"
+file <- file.path(path, "stan", "12", "1.stan")
+mdl <- cmdstanr::cmdstan_model(file, pedantic=TRUE)
+fit <- mdl$sample(data=dat_ls)
+
+
