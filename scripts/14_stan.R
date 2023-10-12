f16e20461291745fe3f16c0b3efe619f6cec165a
Steven Marcel Bißantz <steven.bissantz@gmail.com>
2023-10-12 12:25:26 +0200

    Update 14_stan.R

--- scripts/14_stan.R
+++ scripts/14_stan.R
@@ -192,4 +192,212 @@ for(l in c(0.1,0.3,0.5,0.8,0.99)) {
     lines( ellipse(Sigma_est, centre = Mu_est, level = l), col = "black" )
 }
 
+#
+# Advanced varying slopes model
+#
+library(rethinking)
+data(chimpanzees)
+d <- chimpanzees
+d$block_id <- d$block
+d$treatment <- 1L + d$prosoc_left + 2L*d$condition
+
+dat <- list(
+    L = d$pulled_left,
+    tid = d$treatment,
+    actor = d$actor,
+    block_id = as.integer(d$block_id) )
+
+set.seed(4387510)
+
+m14.2 <- ulam(
+    alist(
+        L ~ dbinom(1,p),
+        logit(p) <- g[tid] + alpha[actor,tid] + beta[block_id,tid],
+        # adaptive priors
+        vector[4]:alpha[actor] ~ multi_normal(0,Rho_actor,sigma_actor),
+        vector[4]:beta[block_id] ~ multi_normal(0,Rho_block,sigma_block),
+        # fixed priors
+        g[tid] ~ dnorm(0,1),
+        sigma_actor ~ dexp(1),
+        Rho_actor ~ dlkjcorr(4),
+        sigma_block ~ dexp(1),
+        Rho_block ~ dlkjcorr(4)
+    ) , data=dat , chains=4 , cores=4 )
+
+stancode(m14.2)
+
+dat_ls <- list(
+    "L" = d$pulled_left,
+    "treatment_id" = d$treatment,
+    "actor_id" = d$actor,
+    "block_id" = as.integer(d$block_id) 
+)
+
+# Fit the model 
+#
+path <- "~/projects/stanmisc"
+file <- file.path(path, "stan", "14", "2.stan")
+mdl <- cmdstanr::cmdstan_model(file, pedantic=TRUE)
+fit <- mdl$sample(data=dat_ls)
+# Note that the alpha parameters are on the log-odds scale!
+fit$print(max_rows=200)
+
+# Diagnostics
+#
+fit$sampler_diagnostics()
+fit$cmdstan_diagnose()
+fit$diagnostic_summary()
+
+# Posterior
+#
+post <- fit$draws(format = "matrix") 
+
+#
+# Cholesky factorization
+#
+
+dat_ls <- list(
+    "L" = d$pulled_left,
+    "tid" = d$treatment,
+    "actor" = d$actor,
+    "block_id" = as.integer(d$block_id) 
+)
+
+m14.3 <- ulam(
+    alist(
+        L ~ binomial(1,p),
+        logit(p) <- g[tid] + alpha[actor,tid] + beta[block_id,tid],
+        # adaptive priors - non-centered
+        transpars> matrix[actor,4]:alpha <-
+                compose_noncentered( sigma_actor , L_Rho_actor , z_actor ),
+        transpars> matrix[block_id,4]:beta <-
+                compose_noncentered( sigma_block , L_Rho_block , z_block ),
+        matrix[4,actor]:z_actor ~ normal( 0 , 1 ),
+        matrix[4,block_id]:z_block ~ normal( 0 , 1 ),
+        # fixed priors
+        g[tid] ~ normal(0,1),
+        vector[4]:sigma_actor ~ dexp(1),
+        cholesky_factor_corr[4]:L_Rho_actor ~ lkj_corr_cholesky( 2 ),
+        vector[4]:sigma_block ~ dexp(1),
+        cholesky_factor_corr[4]:L_Rho_block ~ lkj_corr_cholesky( 2 ),
+        # compute ordinary correlation matrixes from Cholesky factors
+        gq> matrix[4,4]:Rho_actor <<- Chol_to_Corr(L_Rho_actor),
+        gq> matrix[4,4]:Rho_block <<- Chol_to_Corr(L_Rho_block)
+    ) , data=dat_ls , chains=4 , cores=4 , log_lik=TRUE )
+
+stancode(m14.3)
+
+path <- "~/projects/stanmisc"
+file <- file.path(path, "stan", "14", "3.stan")
+mdl <- cmdstanr::cmdstan_model(file, pedantic=TRUE)
+fit <- mdl$sample(data=dat)
+# Note that the alpha parameters are on the log-odds scale!
+fit$print(max_rows=200)
+
+# Further improvmenets (lecture 2023)
+library(rethinking)
+data(chimpanzees)
+d <- chimpanzees
+dat <- list(
+P = d$pulled_left,
+A = as.integer(d$actor),
+B = as.integer(d$block),
+T = 1L + d$prosoc_left + 2L*d$condition)
+
+m14.3 <- ulam(
+alist(
+P ~ bernoulli(p),
+logit(p) <- abar[A] + a[A,T] + bbar[B] + b[B,T],
+# adaptive priors - non-centered
+transpars> matrix[A,4]:a <-
+compose_noncentered( sigma_A , L_Rho_A , zA ),
+transpars> matrix[B,4]:b <-
+compose_noncentered( sigma_B , L_Rho_B , zB ),
+matrix[4,A]:zA ~ normal( 0 , 1 ),
+matrix[4,B]:zB ~ normal( 0 , 1 ),
+zAbar[A] ~ normal(0,1),
+zBbar[B] ~ normal(0,1),
+transpars> vector[A]:abar <<- zAbar*tau_A,
+transpars> vector[B]:bbar <<- zBbar*tau_B,
+# fixed priors
+c(tau_A,tau_B) ~ exponential(1),
+vector[4]:sigma_A ~ exponential(1),
+cholesky_factor_corr[4]:L_Rho_A ~ lkj_corr_cholesky(4),
+vector[4]:sigma_B ~ exponential(1),
+cholesky_factor_corr[4]:L_Rho_B ~ lkj_corr_cholesky(4),
+# compute ordinary correlation matrixes
+gq> matrix[4,4]:Rho_A <<- Chol_to_Corr(L_Rho_A),
+gq> matrix[4,4]:Rho_B <<- Chol_to_Corr(L_Rho_B)
+) , data=dat , chains=4 , cores=4 , log_lik=TRUE )
+
+stancode(m14.3)
+
+#
+# 14.3 Instruments and Causal Design
+#
+set.seed(112)
+# Generative Simulation
+N <- 1e3
+U_sim <- rnorm(N, 0, 1)
+# Quarters
+Q_sim <- sample( 1:4, size=N, replace = TRUE)
+E_sim <- rnorm(N, Q_sim + U_sim)
+# Set the Tx effect (E -> D) to 0
+W_sim <- rnorm(N, 0*E_sim + U_sim)
+
+# Add the (standardized) variables to a list
+dat_sim <- list(
+    N = N,    
+    W = as.numeric(scale(W_sim)),
+    E = as.numeric(scale(E_sim)),
+    Q = as.numeric(scale(Q_sim))
+)
+
+# Statistical model I
+path <- "~/projects/rethinking2nd"
+file <- file.path(path, "stan", "14", "4.stan")
+mdl <- cmdstanr::cmdstan_model(file, pedantic=TRUE)
+fit <- mdl$sample(data=dat_sim)
+fit$cmdstan_diagnose()
+# Note that the alpha parameters are on the log-odds scale!
+fit$print(max_rows=200)
+# Answer: beta_EW = 0.41. The confound does its job!
+
+# Statistical model II
+path <- "~/projects/rethinking2nd"
+file <- file.path(path, "stan", "14", "5.stan")
+mdl <- cmdstanr::cmdstan_model(file, pedantic=TRUE)
+fit <- mdl$sample(data=dat_sim)
+fit$cmdstan_diagnose()
+# Note that the alpha parameters are on the log-odds scale!
+fit$print(max_rows=200)
+# Answer: beta_EW = 0.64. U amplifies the bias of the confound! 
+
+# Statistical model III
+#
+
+# Ulam
+m14.6 <- rethinking::ulam(
+    alist(
+        c(W,E) ~ multi_normal( c(muW,muE) , Rho , Sigma ),
+        muW <- aW + bEW*E,
+        muE <- aE + bQE*Q,
+        c(aW,aE) ~ normal( 0 , 0.2 ),
+        c(bEW,bQE) ~ normal( 0 , 0.5 ),
+        Rho ~ lkj_corr( 2 ),
+        Sigma ~ exponential( 1 )
+    ), data=dat_sim , chains=4 , cores=4 )
+precis( m14.6 , depth=3 )
+rethinking::stancode(m14.6)
+
+# Stan
+path <- "~/projects/rethinking2nd"
+file <- file.path(path, "stan", "14", "6.stan")
+mdl <- cmdstanr::cmdstan_model(file, pedantic=TRUE)
+fit <- mdl$sample(data=dat_sim)
+fit$cmdstan_diagnose()
+# Note that the alpha parameters are on the log-odds scale!
+fit$print(max_rows=200)
+# Answer: beta_EW = 0. The new model deconfounds the estimate
+
 
