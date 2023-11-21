061050c94fbae30d3b7a5ab248453a0608535dfb
Steven Marcel Bißantz <steven.bissantz@gmail.com>
2023-11-21 12:39:18 +0100

    Update 14_stan.R

--- scripts/14_stan.R
+++ scripts/14_stan.R
@@ -400,4 +400,377 @@ fit$cmdstan_diagnose()
 fit$print(max_rows=200)
 # Answer: beta_EW = 0. The new model deconfounds the estimate
 
+########################################################
+# 14.4 Social relations and correlated varying effects #
+########################################################
 
+# Load required packages
+# Note: dagitty is loaded by rethinking
+library(rethinking)
+
+# Get informed
+?KosterLeckie
+
+# Load data
+data(KosterLeckie)
+
+# Data list
+kl_data <- list(
+    N = nrow(kl_dyads),
+    N_households = max(kl_dyads$hidB),
+    did = kl_dyads$did, 
+    hidA = kl_dyads$hidA,
+    hidB = kl_dyads$hidB,
+    giftsAB = kl_dyads$giftsAB,
+    giftsBA = kl_dyads$giftsBA
+    )
+
+# Statistical model I
+
+# Ulam model
+m14.7 <- ulam(
+    alist(
+        giftsAB ~ poisson( lambdaAB ),
+        giftsBA ~ poisson( lambdaBA ),
+        log(lambdaAB) <- a + gr[hidA,1] + gr[hidB,2] + d[did,1] ,
+        log(lambdaBA) <- a + gr[hidB,1] + gr[hidA,2] + d[did,2] ,
+        a ~ normal(0,1),
+       ## gr matrix of varying effects
+        vector[2]:gr[N_households] ~ multi_normal(0,Rho_gr,sigma_gr),
+        Rho_gr ~ lkj_corr(4),
+        sigma_gr ~ exponential(1),
+       ## dyad effects
+        transpars> matrix[N,2]:d <-
+                compose_noncentered( rep_vector(sigma_d,2) , L_Rho_d , z ),
+        matrix[2,N]:z ~ normal( 0 , 1 ),
+        cholesky_factor_corr[2]:L_Rho_d ~ lkj_corr_cholesky( 8 ),
+        sigma_d ~ exponential(1),
+       ## compute correlation matrix for dyads
+        gq> matrix[2,2]:Rho_d <<- Chol_to_Corr( L_Rho_d )
+    ), data=kl_data , chains=4 , cores=4 , iter=2000 )
+
+stancode(m14.7)
+
+# Stan model
+path <- "~/projects/rethinking2nd"
+file <- file.path(path, "stan", "14", "7.stan")
+mdl <- cmdstanr::cmdstan_model(file, pedantic=TRUE)
+fit <- mdl$sample(data=kl_data)
+fit$cmdstan_diagnose()
+# Note that the alpha parameters are on the log-odds scale!
+fit$print(max_rows=200)
+
+#
+# Gaussian process
+#
+
+# Load packages
+library(rethinking)
+
+# Load data
+data(islandsDistMatrix)
+data(Kline2)
+d <- Kline2
+# Number of cases
+N <- nrow(d)
+# Indicator variable
+d$society <- seq(N)
+
+# Distance matrix 
+Dmat <- islandsDistMatrix
+colnames(Dmat) <- abbreviate(rownames(Dmat), 2)
+round(Dmat, 1) # Distance between island i and j
+
+# Covariance function
+# ...relates correlation to distance
+curve( exp(-x), from = 0, to = 4, lwd = 3) # Linear (L1)
+curve( exp(-x^2), from = 0, to = 4, lwd = 3, col = "steelblue",
+    add = TRUE) # Squared exponential (L2)
+
+# Data list
+dat_list <- list(
+    T = d$total_tools,
+    P = d$population,
+    society = d$society,
+    Dmat = islandsDistMatrix
+)
+
+# Ulam 
+m14.8 <- ulam(
+    alist(
+        T ~ dpois(lambda),
+        lambda <- (a*P^b/g)*exp(k[society]),
+        vector[10]:k ~ multi_normal( 0 , SIGMA ),
+        matrix[10,10]:SIGMA <- cov_GPL2( Dmat , etasq , rhosq , 0.01 ),
+        c(a,b,g) ~ dexp( 1 ),
+        etasq ~ dexp( 2 ),
+        rhosq ~ dexp( 0.5 )
+    ), data=dat_list , chains=4 , cores=4 , iter=2000 )
+
+precis(m14.8, depth = 3)
+stancode(m14.8)
+
+# Data list
+dat_list <- list(
+    N = nrow(d),
+    T = d$total_tools,
+    P = d$population,
+    society = d$society,
+    Dmat = islandsDistMatrix
+)
+
+path <- "~/projects/rethinking2nd"
+file <- file.path(path, "stan", "14", "8a.stan")
+mdl <- cmdstanr::cmdstan_model(file, pedantic=TRUE)
+fit <- mdl$sample(data=dat_list)
+fit$print(max_rows=200)
+
+# 8b does not provide the same pproximation, because values have to be
+# pre-set. Solution in 8a is much more elegant
+#path <- "~/projects/rethinking2nd"
+#file <- file.path(path, "stan", "14", "8b.stan")
+#mdl <- cmdstanr::cmdstan_model(file, pedantic=TRUE)
+#fit <- mdl$sample(data=dat_list)
+#Note that the alpha parameters are on the log-odds scale!
+#fit$print(max_rows=200)
+
+fit$sampler_diagnostics()
+fit$diagnostic_summary()
+
+# Posterior draws
+praws <- fit$draws(format = "data.frame")
+
+# Emptyplot
+plot(NULL, xlab = "Distance (thousand km)", ylab = "covariance",
+xlim = c(0,10), ylim = c(0,0.5))
+# Compute posterior mean covariance
+x_seq <- seq(0, 10, length.out = 100 )
+# Posterior mean covariances
+pmcov <- sapply( x_seq, function(x) praws$etasq * exp(-praws$rhosq*x^2) )
+pmcov_mu <- apply( pmcov, 2, mean)
+# Mean relationsship between covariance and distance
+lines( x_seq, pmcov_mu, lwd = 5, col = "steelblue" )
+# Relationsship between covariance and distance
+for(i in 1:50) {
+    curve(praws$etasq[i] * exp(-praws$rhosq[i]*x^2), add = TRUE
+    )
+}
+
+# Compute the posterior median covariance among societies 
+K <- matrix(0, nrow = 10, ncol = 10)
+for (i in 1:10) {
+    for(j in 1:10) {
+        K[i,j] <- median(praws$etasq) * exp (-median(praws$rhosq) * islandsDistMatrix[i,j]^2)
+    }
+}
+diag(K) <- median(praws$etasq) + 0.01
+# Convert to correlation matrix
+Rho <- round( cov2cor(K), 2 )
+rownames(Rho) <- colnames(Dmat)
+colnames(Rho) <- colnames(Dmat)
+
+# scale point size to logpop
+psize <- d$logpop / max(d$logpop)
+psize <- exp(psize*1.5)-2
+# plot raw data and labels
+plot( d$lon2 , d$lat , xlab="longitude" , ylab="latitude" ,
+    col=rangi2 , cex=psize , pch=16 , xlim=c(-50,30) )
+labels <- as.character(d$culture)
+text( d$lon2 , d$lat , labels=labels , cex=0.7 , pos=c(2,4,3,3,4,1,3,2,4,2) )
+# overlay lines shaded by Rho
+for( i in 1:10 )
+    for ( j in 1:10 )
+        if ( i < j )
+            lines( c( d$lon2[i],d$lon2[j] ) , c( d$lat[i],d$lat[j] ) ,
+                lwd=2 , col=col.alpha("black",Rho[i,j]^2) )
+
+
+colnames(praws)
+# compute posterior median relationship, ignoring distance
+logpop.seq <- seq( from=6 , to=14 , length.out=30 )
+lambda <- sapply( logpop.seq , function(lp) exp( praws$a + praws$b*lp ) )
+lambda.median <- apply( lambda , 2 , median )
+lambda.PI80 <- apply( lambda , 2 , PI , prob=0.8 )
+# plot raw data and labels
+plot( d$logpop , d$total_tools , col=rangi2 , cex=psize , pch=16 ,
+    xlab="log population" , ylab="total tools" )
+text( d$logpop , d$total_tools , labels=labels , cex=0.7 ,
+    pos=c(4,3,4,2,2,1,4,4,4,2) )
+# display posterior predictions
+lines( logpop.seq , lambda.median , lty=2 )
+lines( logpop.seq , lambda.PI80[1,] , lty=2 )
+lines( logpop.seq , lambda.PI80[2,] , lty=2 )
+# overlay correlations
+for( i in 1:10 )
+    for ( j in 1:10 )
+        if ( i < j )
+            lines( c( d$logpop[i],d$logpop[j] ) ,
+       c( d$total_tools[i],d$total_tools[j] ) ,
+       lwd=2 , col=col.alpha("black",Rho[i,j]^2) )
+
+# Non-centered Gaussian Process
+#
+m14.8nc <- ulam(
+    alist(
+        T ~ dpois(lambda),
+        lambda <- (a * P^b / g) * exp(k[society]),
+        # non-centered Gaussian Process prior
+        transpars > vector[10]:k <<- L_SIGMA * z,
+        vector[10]:z ~ normal(0, 1),
+        transpars > matrix[10, 10]:L_SIGMA <<- cholesky_decompose(SIGMA),
+        transpars > matrix[10, 10]:SIGMA <- cov_GPL2(Dmat, etasq, rhosq, 0.01),
+        c(a, b, g) ~ dexp(1),
+        etasq ~ dexp(2),
+        rhosq ~ dexp(0.5)
+    ),
+    data = dat_list, chains = 4, cores = 4, iter = 2000
+)
+stancode(m14.8nc)
+
+path <- "~/projects/rethinking2nd"
+file <- file.path(path, "stan", "14", "8a_nc.stan")
+mdl <- cmdstanr::cmdstan_model(file, pedantic=TRUE)
+fit <- mdl$sample(data=dat_list)
+fit$print(max_rows=200)
+
+# Diagnostics
+fit$sampler_diagnostics()
+fit$diagnostic_summary()
+
+# Posterior draws
+praws <- fit$draws(format = "data.frame")
+praws
+
+# Phylogenetic distance
+#
+library(rethinking)
+data(Primates301)
+data(Primates301_nex)
+
+# Visualize phylogeny 
+library(ape)
+plot( ladderize(Primates301_nex), type = "fan", cex = .5)
+
+# Load the data
+d <- Primates301
+d$name <- as.character(d$name)
+dstan <- d[complete.cases( d$group_size, d$body, d$brain),]
+spp_obs <- dstan$name
+
+# Create the data list
+dat_list <- list(
+    N_spp = nrow(dstan),
+    M = standardize( log(dstan$body) ),
+    B = standardize( log(dstan$brain) ),
+    G = standardize( log(dstan$group_size) ),
+    Imat = diag( nrow(dstan) )
+)
+
+m14.9 <- ulam(
+    alist(
+        B ~ multi_normal(mu, K),
+        mu <- a + bM * M + bG * G,
+        matrix[N_spp, N_spp]:K <- Imat * sigma_sq,
+        a ~ normal(0, 1),
+        c(bM, bG) ~ normal(0, 0.5),
+        sigma_sq ~ exponential(1)
+    ),
+    data = dat_list, chains = 4, cores = 4
+)
+precis( m14.9 )
+
+# Stancode
+stancode(m14.9)
+
+# Stan Version
+#
+path <- "~/projects/rethinking2nd"
+file <- file.path(path, "stan", "14", "9.stan")
+mdl <- cmdstanr::cmdstan_model(file, pedantic=TRUE)
+fit <- mdl$sample(data=dat_list)
+fit$print(max_rows=200)
+
+# Build the distance matrix 
+#
+library(ape)
+tree_trimmed <- keep.tip( Primates301_nex, spp_obs )
+Rbm <- corBrownian( phy=tree_trimmed )
+V <- vcv(Rbm)
+Dmat <- cophenetic( tree_trimmed )
+plot( Dmat, V, xlab="pyhlogenetic distance", ylab = "covariance")
+image(V)
+image(Dmat)
+
+# Put species in right order
+dat_list$V <- V[spp_obs, spp_obs]
+# Convert to correlation matrix
+dat_list$R <- dat_list$V / max(V)
+
+m14.10 <- ulam(
+    alist(
+        B ~ multi_normal(mu, K),
+        mu <- a + bM * M + bG * G,
+        matrix[N_spp, N_spp]:K <- R * sigma_sq,
+        a ~ normal(0, 1),
+        c(bM, bG) ~ normal(0, 0.5),
+        sigma_sq ~ exponential(1)
+    ),
+    data = dat_list, chains = 4, cores = 4
+)
+precis( m14.10 )
+
+# Extract Stancode
+stancode(m14.10)
+
+# Stan version
+#
+path <- "~/projects/rethinking2nd"
+file <- file.path(path, "stan", "14", "10.stan")
+mdl <- cmdstanr::cmdstan_model(file, pedantic=TRUE)
+fit <- mdl$sample(data=dat_list)
+fit$print(max_rows=200)
+
+# Ornstein-Uhlbeck Kernel (L1 norm)
+
+# Scaled and reordered distance matrix
+dat_list$Dmat <- Dmat[ spp_obs, spp_obs ] / max(Dmat)
+m14.11 <- ulam(
+    alist(
+        B ~ multi_normal(mu, K),
+        mu <- a + bM * M + bG * G,
+        matrix[N_spp, N_spp]:K <- cov_GPL1(Dmat, etasq, rhosq, 0.01),
+        a ~ normal(0, 1),
+        c(bM, bG) ~ normal(0, 0.5),
+        etasq ~ half_normal(1, 0.25),
+        rhosq ~ half_normal(3, 0.25)
+    ),
+    data = dat_list, chains = 4, cores = 4
+)
+precis(m14.11)
+
+# Stan version
+#
+path <- "~/projects/rethinking2nd"
+file <- file.path(path, "stan", "14", "11.stan")
+mdl <- cmdstanr::cmdstan_model(file, pedantic=TRUE)
+fit <- mdl$sample(data=dat_list)
+fit$print(max_rows=200)
+
+# Plot the posteriror covariance function
+#
+post <- fit$draws(format = "data.frame")
+plot(NULL, xlim = c(0, max(dat_list$Dmat)), ylim=c(0,1.5))
+with(post,
+    for( i in 1:30 ) {
+        # Posterior
+        curve( etasq[i]*exp(-rhosq[i]*x), add = TRUE, col = "steelblue")
+    }
+)
+# Prior
+eta <- abs( rnorm(1e3, 1, 0.25) ) #half-normal
+rho <- abs( rnorm(1e3, 3, 0.25) ) #half-normal
+d_seq <- seq(0, 5, length.out = 1e3)
+K <- sapply(d_seq, function(x) eta*exp(-rho*x))
+# Mean
+lines( d_seq, colMeans(K), lwd = 2)
+shade( apply(K, 2, PI), d_seq)
