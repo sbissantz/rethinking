d2f60d46bd2fb063be754ba8af246c785d7abc17
Steven Marcel Bißantz <steven.bissantz@gmail.com>
2024-03-21 11:12:26 +0100

    Update lecture_18.r

--- rethinking22/lecture_18.r
+++ rethinking22/lecture_18.r
@@ -118,10 +118,10 @@ plot(density(draws1a$bG), lwd = 3, col = "steelblue", xlab = "effect of G on B",
 ylim = c(0, 25)) 
 abline(v = 0, lty = 3)
 
-# Impute group size (1b.stan)
+# Impute group size and body mass (1b.stan)
 
 # Data
-dd <- d[complete.cases(d$brain, d$body), ]
+dd <- d[complete.cases(d$brain),]
 spp <- dd$name
 # Phylogeny
 spp <- as.character(dd$name)
@@ -130,14 +130,22 @@ Rbm <- corBrownian(phy = tree_trimmed)
 V <- vcv(Rbm)
 Dmat <- cophenetic(tree_trimmed)
 
-# Impute only group size
+
 stan_ls <- list(
     N = nrow(dd),
+    # Impute G
     N_G_obs = sum(complete.cases(dd$group_size)),
-    ii_G_obs = which(complete.cases(dd$group_size)),
     N_G_mis = sum(is.na(dd$group_size)),
+    G_obs = standardize(log(dd$group_size[complete.cases(dd$group_size)])),
+    ii_G_obs = which(complete.cases(dd$group_size)),
     ii_G_mis = which(is.na(dd$group_size)),
-    M = standardize(log(dd$body)),
+    # Impute B
+    N_M_obs = sum(complete.cases(dd$body)),
+    N_M_mis = sum(is.na(dd$body)),
+    M_obs = standardize(log(dd$body[complete.cases(dd$body)])),
+    ii_M_obs = which(complete.cases(dd$body)),
+    ii_M_mis = which(is.na(dd$body)),
+    # Oldje stuff 
     B = standardize(log(dd$brain)),
     Dmat = Dmat[spp, spp] / max(Dmat)
 )
@@ -154,5 +162,65 @@ draws1b <- fit$draws(format = "data.frame")
 
 colnames(draws1b)
 
+#
+# Why is the other model running so much faster????
+#
+
 
 # Continue with two missing variables (1c.stan)
+
+## R code 14.49
+data(Primates301)
+d <- Primates301
+d$name <- as.character(d$name)
+dstan <- d[ complete.cases( d$group_size , d$body , d$brain ) , ]
+spp_cc <- dstan$name
+
+# complete case analysis
+dstan <- d[complete.cases(d$group_size,d$body,d$brain),]
+dat_cc <- list(
+    N_spp = nrow(dstan),
+    M = standardize(log(dstan$body)),
+    B = standardize(log(dstan$brain)),
+    G = standardize(log(dstan$group_size)),
+    Imat = diag(nrow(dstan)) )
+
+# drop just missing brain cases
+dd <- d[complete.cases(d$brain),]
+dat_all <- list(
+    N_spp = nrow(dd),
+    M = standardize(log(dd$body)),
+    B = standardize(log(dd$brain)),
+    G = standardize(log(dd$group_size)),
+    Imat = diag(nrow(dd)) )
+
+dd <- d[complete.cases(d$brain),]
+table( M=!is.na(dd$body) , G=!is.na(dd$group_size) )
+
+# now with phylogeny
+
+library(ape)
+spp <- as.character(dd$name)
+tree_trimmed <- keep.tip( Primates301_nex, spp )
+Rbm <- corBrownian( phy=tree_trimmed )
+V <- vcv(Rbm)
+Dmat <- cophenetic( tree_trimmed )
+
+# distance matrix
+dat_all$Dmat <- Dmat[ spp , spp ] / max(Dmat)
+
+# imputation ignoring models of M and G
+fMBG_OU <- alist(
+    B ~ multi_normal( mu , K ),
+    mu <- a + bM*M + bG*G,
+    M ~ normal(0,1),
+    G ~ normal(0,1),
+    matrix[N_spp,N_spp]:K <- cov_GPL1(Dmat,etasq,rho,0.01),
+    a ~ normal( 0 , 1 ),
+    c(bM,bG) ~ normal( 0 , 0.5 ),
+    etasq ~ half_normal(1,0.25),
+    rho ~ half_normal(3,0.25)
+)
+mBMG_OU <- ulam( fMBG_OU , data=dat_all , chains=4 , cores=4 , sample=TRUE )
+
+stancode( mBMG_OU )
